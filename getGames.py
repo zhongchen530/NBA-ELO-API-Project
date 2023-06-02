d26bacb5f3e61e77a9491a37c920b44e37c1ba9f
Zhong <zhongyuanchen@zhongyuans-MBP.lan>
2023-06-01 20:01:12 -0400

    replace SQL raw string with ORM

---
+++ getGames.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  1 10:24:54 2023
+
+@author: zhongyuanchen
+"""
+from nba_api.stats.static import teams
+from nba_api.stats.endpoints import leaguegamefinder
+import pandas as pd
+
+columns = ['SEASON_ID', 'TEAM_ID', 'TEAM_ABBREVIATION', 'TEAM_NAME', 'GAME_ID',
+       'GAME_DATE', 'MATCHUP', 'WL']
+
+def get_range(date_range):
+    if len(date_range) != 2:
+        raise Exception("Expected date_range with length 2,%s given"%len(date_range))
+        
+    start,end = date_range
+    teams_df = []
+    for team in teams.get_teams():
+        team_id = team["id"]
+        team_games = leaguegamefinder.LeagueGameFinder(team_id_nullable = team_id)
+        team_df = team_games.get_data_frames()[0]
+        team_df = team_df[columns]
+        filt = (start <= team_df["GAME_DATE"]) & (team_df["GAME_DATE"] < end)
+        team_df = team_df[filt]
+        teams_df.append(team_df)
+
+    teams_df = pd.concat(teams_df,axis = 0)
+    return teams_df
+    
---
+++ writeGames.py
@@ -0,0 +1,56 @@
+import pymysql
+import mysql.connector
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, ForeignKey,Column,String,INTEGER
+import psycopg2 as pg
+
+base = declarative_base()
+
+class Game(base):
+    __tablename__ = "games"
+    
+    SEASON_ID = Column("season_id",INTEGER)
+    TEAM_ID = Column("team_id",INTEGER)
+    TEAM_ABBREVIATION = Column("team_abbreviation",String)
+    TEAM_NAME = Column("team_name",String)
+    GAME_ID = Column("game_id",INTEGER,primary_key = True)
+    GAME_DATE = Column("game_date",String)
+    MATCHUP = Column("matchup",String)
+    WL = Column("wl",String)
+    
+    def __init__(self,season_id,team_id,team_abbreviation,team_name,game_id,game_date,matchup,wl):
+        self.SEASON_ID = season_id
+        self.TEAM_ID = team_id
+        self.TEAM_ABBREVIATION = team_abbreviation
+        self.TEAM_NAME = team_name
+        self.GAME_ID = game_id
+        self.GAME_DATE = game_date
+        self.MATCHUP = matchup
+        self.WL = wl
+        
+def get_connection(user,password,host,port,database):
+    return create_engine(
+        url="mysql+pymysql://{0}:{1}@{2}:{3}/{4}".format(
+            user, password, host, port, database
+        )
+    )
+    
+def write_to_db(df,user,password,host,port,database):
+    eingine = get_connection(user, password, host, port, database)
+    return
+
+
+    sql = '''
+    create table if not exists GAMES(
+    SEASON_ID varchar(20),
+    TEAM_ID varchar(20),
+    TEAM_ABBREVIATION varchar(6),
+    TEAM_NAME varchar(20),
+    GAME_ID varchar(20),
+    GAME_DATE varchar(12),
+    MATCHUP varchar(20),
+    WL varchar(1)
+    )
+    '''
